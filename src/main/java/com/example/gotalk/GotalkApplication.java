9b17680eedbf37588c9066a87f8eca402d581fe4
gyj <gyj@gmail.com>
2024-11-05 16:33:01 +0900

    created project

---
+++ src/main/java/com/example/gotalk/GotalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.gotalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gotalk/GotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
