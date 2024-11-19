7466096aba22cf5ee17005813d790295e044b91f
gyj <gyj@gmail.com>
2024-11-19 12:00:51 +0900

    시큐리티 설정 -> 개발을 위해 일부 url 접근 제어 해지

---
+++ src/main/java/com/example/gotalk/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.gotalk.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring()
+                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        };
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        // Ant 경로 패턴을 명시적으로 사용
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/"),
+                                AntPathRequestMatcher.antMatcher("/favicon.ico"),
+                                AntPathRequestMatcher.antMatcher("/login"),
+                                AntPathRequestMatcher.antMatcher("/sign-up"),
+                                AntPathRequestMatcher.antMatcher("/check-email-token"),
+                                AntPathRequestMatcher.antMatcher("/email-login"),
+                                AntPathRequestMatcher.antMatcher("/login-by-email"),
+                                AntPathRequestMatcher.antMatcher("/search/study")).permitAll()
+                        .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET,"/profile/*")).permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+
+}
